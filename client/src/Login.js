f19c1d8584235cf3755a6eb9fa98d631dc0341c5
wadewilsones <uladaharanina@gmail.com>
2022-04-13 18:56:18 -0400

    Sign up Fixed

--- client/src/Login.js
+++ client/src/Login.js
@@ -12,7 +12,9 @@ function Login (){
     const changeInput = (event) => {
         const { name, value } = event.target;
         setUserdata(
-            (prevState) => ({...prevState, [name]: value}), console.log(value))      
+            (prevState) => (
+                {...prevState, [name]: value}
+            ))
     }
 
     const {username, password} = userData;
@@ -49,7 +51,7 @@ function Login (){
             <label htmlFor="username">Username</label>
            <input type="text" id="username" name="username"  value = {username} onChange = {changeInput} placeholder='Type here..' required />
            <label htmlFor="password">Password</label>
-           <input type="text" id="password" name="password" value = {password} onChange = {changeInput} placeholder='Type here..' required />
+           <input type="password" id="password" name="password" value = {password} onChange = {changeInput} placeholder='Type here..' required />
            <input type="submit" id="subBtn" value="Login"/>
            <p>Don't have an account? <Link to="/sign-up">Sign Up!</Link></p>
 
@@ -58,81 +60,5 @@ function Login (){
     )
 }
 
-/*
-class Login extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            username:"",
-            password:""
-        }
-
-         this.handleSubmit = this.handleSubmit.bind(this);
-        this.handleChange = this.handleChange.bind(this);
-        this.sendData = this.sendData.bind(this);
-    }
-
-    handleChange(event){
-        const target = event.target;
-        const value = target.value;
-        const name = target.name;
-        this.setState(
-            {
-                [name]:value
-            }
-        )
-        
-    }
-
-    handleSubmit(e){
-        e.preventDefault();
-        console.log();
-        if (this.state.username != null && this.state.password != null)
-        {
-            this.sendData();
-        }
-
-    }
-
-    sendData(){
-        const formData = {
-            username:this.state.username,
-            password:this.state.password
-        }
-
-        fetch('/login', {
-            method:'POST',
-            headers:{'Content-Type':'application/json'},
-            body: JSON.stringify(formData)
-        })
-
-        .then(response => response.json())
-        .then(data => console.log(data))
-
-    }
-
-
-    render(){
-        return(
-            <div className="sign-up-container">
-                <h1>Moodnex</h1>
-                <h5>Your personal mood app</h5>
-                <p>Login</p>
-               <form method="POST" action="/" onSubmit = {this.handleSubmit}> 
-                    <label htmlFor="username">Username</label>
-                   <input type="text" id="username" name="username" value={this.state.username} onChange = {this.handleChange} placeholder='Type here..' required />
-                   <label htmlFor="password">Password</label>
-                   <input type="text" id="password" name="password" value={this.state.password} onChange = {this.handleChange} placeholder='Type here..' required />
-                   <input type="submit" id="subBtn" value="Login"/>
-                   <p>Don't have an account? <Link to="/sign-up">Sign Up!</Link></p>
-
-               </form>
-            </div>
-
-        )
-    }
-
-}
-*/
 
 export default Login;
--- client/src/Sign-Up.js
+++ client/src/Sign-Up.js
@@ -1,106 +1,66 @@
 import './App.css';
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 
+function SignUp (){
 
-class SignUp extends React.Component{
+    const [userData, setUserdata] = useState({
+        username:'',
+        password:''
+    });
 
-    constructor(props){
-        super(props);
-        this.state = {
-            username:'',
-            password:''
-        };
-
-        this.validateForm = this.validateForm.bind(this);
-        this.processForm = this.processForm.bind(this);
-        this.sendData = this.sendData.bind(this);
-        this.thankNotif = this.thankNotif.bind(this);
-        this.closeNote = this.closeNote.bind(this);
-
+    const changeInput = (event) => {
+        const { name, value } = event.target;
+        setUserdata(
+            (prevState) => ({...prevState, [name]: value}),
+            userData.password = btoa(userData.password)
+            )
+            console.log(userData.password);
     }
 
-    validateForm(e){
-       e.preventDefault();
-       if(document.getElementById('password').value.length > 6){
-           let encryptpsw = btoa(document.getElementById('password').value);
-           this.processForm(encryptpsw);
-       }
-       else{
-           alert('Minimum 8 characters');
-       }
-    }
+    const {username, password} = userData;
 
-    processForm(psw){
 
-        this.setState(
-            {username: document.getElementById('username').value,
-            password:  psw
-
-        }, () =>  {this.sendData()}
-        )
-    }
-
-     sendData(){
-         const formData = {
-            username:this.state.username,
-            password:this.state.password,
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log();
+        if (username != null && password != null)
+        {
+            sendData();
         }
+    }
+
+    const sendData = () => {
 
         fetch('/signUp', {
             method:'POST',
             headers:{'Content-Type':'application/json'},
-            body:JSON.stringify(formData)
+            body: JSON.stringify(userData)
         })
 
         .then(response => response.json())
-        .then(data => {
-            console.log(data)
-            this.thankNotif()})
+        .then(data => console.log(data))
+
     }
 
-    thankNotif(){
-      document.getElementById('thanksNote').style.display = 'block';
-       document.getElementById('sign-up-container').style.filter = "blur(1.5rem)"; 
-    }
+    return(
+        <div className="sign-up-container">
+        <h1>Moodnex</h1>
+        <h5>Your personal mood app</h5>
+        <p>Sign Up</p>
+       <form method="POST" action="/" onSubmit={handleSubmit}> 
+            <label htmlFor="username">Username</label>
+           <input type="text" id="username" name="username"  value = {username} onChange = {changeInput} placeholder='Username..' required />
+           <label htmlFor="passwordR">Password</label>
+           <input type="password" id="passwordR" name="password" value = {password}  onChange = {changeInput} placeholder= 'Enter Your Password' required />
+           <input type="submit" id="subBtn" value="Sign Up"/>
+           <p>Already have an account? <Link to="/login">Log In!</Link></p>
 
-    closeNote(){
-        document.getElementById('thanksNote').style.display = 'none';
-        document.getElementById('sign-up-container').style.filter = "blur(0rem)";
-    
+       </form>
+    </div>
+    )
 }
 
-    render(){
-      
-        return(
-            <div id="sign-up">
-                 <div id='thanksNote'>
-                    <h3>Thank you for registration, {this.state.username}</h3>
-                    <button id='close-note-btn' onClick={this.closeNote}>Close</button>
-             </div>
-            <div className="sign-up-container" id='sign-up-container'>
-                <h1>Moodnex</h1>
-                <h5>Your personal mood app</h5>
-                <p>Sign Up</p>
-                <form method="POST" action = "/" onSubmit={this.validateForm}> 
-                    <label htmlFor="username">Username</label>
-                   <input type="text" id="username" name="username" placeholder='Type here..' required></input>
-                   <label htmlFor="password">Password</label>
-                   <input type="text" id="password" name="password" placeholder='Type here..' required></input>
-                   <input type="submit" id="subBtn" value="Sign Up" name="Submit"/>
-                   <p>Already have an account?  <Link to="/login">Log in!</Link></p>
-               </form>
-
-               </div>
-
-            </div>
-
-        )
-    }
-
-}
 
 export default SignUp;
-
-
