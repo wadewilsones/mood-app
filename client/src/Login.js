104af9c37e543432a1937daf38c16e32299e6999
Ulada <70239279+wadewilsones@users.noreply.github.com>
2022-04-13 17:41:46 -0400

    Update Login.js
    The component was turned into a function. Use state hook was used.

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,52 +1,38 @@
 import './App.css';
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 
-class Login extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            username:"",
-            password:""
-        }
+function Login (){
 
-         this.handleSubmit = this.handleSubmit.bind(this);
-        this.handleChange = this.handleChange.bind(this);
-        this.sendData = this.sendData.bind(this);
+    const [userData, setUserdata] = useState({
+        username:'',
+        password:''
+    });
+
+    const changeInput = (event) => {
+        const { name, value } = event.target;
+        setUserdata(
+            (prevState) => ({...prevState, [name]: value}), console.log(value))      
     }
 
-    handleChange(event){
-        const target = event.target;
-        const value = target.value;
-        const name = target.name;
-        this.setState(
-            {
-                [name]:value
-            }
-        )
-        
-    }
+    const {username, password} = userData;
 
-    handleSubmit(e){
+
+    const handleSubmit = (e) => {
         e.preventDefault();
         console.log();
-        if (this.state.username != null && this.state.password != null)
+        if (username != null && password != null)
         {
-            this.sendData();
+            sendData();
         }
-
     }
 
-    sendData(){
-        const formData = {
-            username:this.state.username,
-            password:this.state.password
-        }
+    const sendData = () => {
 
         fetch('/login', {
             method:'POST',
             headers:{'Content-Type':'application/json'},
-            body: JSON.stringify(formData)
+            body: JSON.stringify(userData)
         })
 
         .then(response => response.json())
@@ -54,27 +40,23 @@ class Login extends React.Component{
 
     }
 
+    return(
+        <div className="sign-up-container">
+        <h1>Moodnex</h1>
+        <h5>Your personal mood app</h5>
+        <p>Login</p>
+       <form method="POST" action="/" onSubmit={handleSubmit}> 
+            <label htmlFor="username">Username</label>
+           <input type="text" id="username" name="username"  value = {username} onChange = {changeInput} placeholder='Type here..' required />
+           <label htmlFor="password">Password</label>
+           <input type="text" id="password" name="password" value = {password} onChange = {changeInput} placeholder='Type here..' required />
+           <input type="submit" id="subBtn" value="Login"/>
+           <p>Don't have an account? <Link to="/sign-up">Sign Up!</Link></p>
 
-    render(){
-        return(
-            <div className="sign-up-container">
-                <h1>Moodnex</h1>
-                <h5>Your personal mood app</h5>
-                <p>Login</p>
-               <form method="POST" action="/" onSubmit = {this.handleSubmit}> 
-                    <label htmlFor="username">Username</label>
-                   <input type="text" id="username" name="username" value={this.state.username} onChange = {this.handleChange} placeholder='Type here..' required />
-                   <label htmlFor="password">Password</label>
-                   <input type="text" id="password" name="password" value={this.state.password} onChange = {this.handleChange} placeholder='Type here..' required />
-                   <input type="submit" id="subBtn" value="Login"/>
-                   <p>Don't have an account? <Link to="/sign-up">Sign Up!</Link></p>
-
-               </form>
-            </div>
-
-        )
-    }
-
+       </form>
+    </div>
+    )
 }
+
 
 export default Login;
