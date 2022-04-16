ef036ef0cf535c9a3300e63b20dedaa8263057b5
Ulada <70239279+wadewilsones@users.noreply.github.com>
2022-04-16 00:11:41 -0400

    Update Login.js
    A login status request was added.

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,8 +1,20 @@
 import './App.css';
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link, NavLink } from "react-router-dom";
 
 function Login (){
+
+const [loginStatus, setLoginStatus] = useState("");
+    
+  useEffect(()=>{
+    fetch("/login", {
+      method:"GET",
+      headers:{'Content-Type':'application/json'},
+      credentials:'include'
+    })
+    .then (response => response.json())
+    .then(data => console.log(data))
+},[])
 
     const [userData, setUserdata] = useState({
         username:'',
@@ -12,7 +24,9 @@ function Login (){
     const changeInput = (event) => {
         const { name, value } = event.target;
         setUserdata(
-            (prevState) => ({...prevState, [name]: value}), console.log(value))      
+            (prevState) => (
+                {...prevState, [name]: value}
+            ))
     }
 
     const {username, password} = userData;
@@ -40,16 +54,19 @@ function Login (){
 
     }
 
+    
+
     return(
         <div className="sign-up-container">
         <h1>Moodnex</h1>
+        <h2>{loginStatus}</h2>
         <h5>Your personal mood app</h5>
         <p>Login</p>
        <form method="POST" action="/" onSubmit={handleSubmit}> 
             <label htmlFor="username">Username</label>
            <input type="text" id="username" name="username"  value = {username} onChange = {changeInput} placeholder='Type here..' required />
            <label htmlFor="password">Password</label>
-           <input type="text" id="password" name="password" value = {password} onChange = {changeInput} placeholder='Type here..' required />
+           <input type="password" id="password" name="password" value = {password} onChange = {changeInput} placeholder='Type here..' required />
            <input type="submit" id="subBtn" value="Login"/>
            <p>Don't have an account? <Link to="/sign-up">Sign Up!</Link></p>
 
