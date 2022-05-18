b316f339b5b222b20d5e05c49f844c671ccaa44c
wadewilsones <uladaharanina@gmail.com>
2022-05-18 18:07:05 -0400

    Design is done

--- client/src/AddMood.js
+++ client/src/AddMood.js
@@ -8,7 +8,7 @@ let AddMood = (props) =>{
 
 let [ mood, setMood ] = useState('');
 let [ symptoms, setSymptoms ] = useState('');
-let [ confirmation, setConfirmation] = useState(true);
+let [ confirmation, setConfirmation] = useState(false);
 
 let userData = {
     userId:props.userId,
@@ -51,56 +51,58 @@ let MoodContainer = {
     return(
         
       <div id='addModd-container'>
-        <Header location = 'addMood' />
-        <section id="confirmation">
-            {confirmation? <div id="Mood-update-note"><h4>A new entry was added</h4></div> : <div></div>}
-        </section>
-        <section id='moodHolder'>
-            <h3>Today I feel: </h3>
-            <div onClick = {handleMood} id = {MoodContainer[0]} >
-                <img src = 'media/happy.svg' alt ='happy' className = {mood === MoodContainer[0]? 'mood-selected' :'mood-img'}/>
-                <h4>{MoodContainer[0]}</h4>
-            </div>
-            <div onClick = {handleMood} id = {MoodContainer[1]} >
-                <img src = 'media/joyful.svg' alt ='Joyful'  className = {mood === MoodContainer[1]? 'mood-selected' :'mood-img'}/>
-                <h4>{MoodContainer[1]}</h4>
-            </div>
-            <div  onClick = {handleMood} id = {MoodContainer[2]} >
-                <img src = 'media/Peaceful.svg' alt ='Peaceful'  className = {mood === MoodContainer[2]? 'mood-selected' :'mood-img'}/>
-                <h4>{MoodContainer[2]}</h4>
-            </div>
-            <div onClick = {handleMood} id = {MoodContainer[3]}>
-                <img src = 'media/loving.svg' alt ='Loving'  className = {mood === MoodContainer[3]? 'mood-selected' :'mood-img'}/>
-                <h4>{MoodContainer[3]}</h4>
-            </div>
-            <div onClick = {handleMood} id = {MoodContainer[4]}>
-                <img src = 'media/stressed.svg' alt ='stressed' className = {mood === MoodContainer[4]? 'mood-selected' :'mood-img'} />
-                <h4>{MoodContainer[4]}</h4>
-            </div>
-            <div onClick = {handleMood} id = {MoodContainer[5]}>
-                <img src = 'media/upset.svg' alt ='upset' className = {mood === MoodContainer[5]? 'mood-selected' :'mood-img'} />
-                <h4>{MoodContainer[5]}</h4>
-            </div>
-            <div  onClick = {handleMood} id = {MoodContainer[6]}>
-                <img src = 'media/angry.svg' alt ='angry'  className = {mood === MoodContainer[6]? 'mood-selected' :'mood-img'}/>
-                <h4>{MoodContainer[6]}</h4>
-            </div>
-            <div onClick = {handleMood} id = {MoodContainer[7]}>
-                <img src = 'media/frustrated.svg' alt ='frustrated'  className = {mood === MoodContainer[7]? 'mood-selected' :'mood-img'}/>
-                <h4>{MoodContainer[7]}</h4>
-            </div>
-            <div onClick = {handleMood} id = {MoodContainer[8]}>
-                <img src = 'media/grumpy.svg' alt ='grumpy'  className = {mood === MoodContainer[8]? 'mood-selected' :'mood-img'} />
-                <h4>{MoodContainer[8]}</h4>
-            </div>
+        <div id = 'addModd-mood-inner-container'>
+            <Header location = 'addMood' />
+            <section id="confirmation">
+                {confirmation? <div id="Mood-update-note"><h4>A new entry was added</h4></div> : <div></div>}
+            </section>
+            <section id='moodHolder'>
+                <h3>Today I feel: </h3>
+                <div onClick = {handleMood} id = {MoodContainer[0]} >
+                    <img src = 'media/happy.svg' alt ='happy' className = {mood === MoodContainer[0]? 'mood-selected' :'mood-img'}/>
+                    <h4>{MoodContainer[0]}</h4>
+                </div>
+                <div onClick = {handleMood} id = {MoodContainer[1]} >
+                    <img src = 'media/joyful.svg' alt ='Joyful'  className = {mood === MoodContainer[1]? 'mood-selected' :'mood-img'}/>
+                    <h4>{MoodContainer[1]}</h4>
+                </div>
+                <div  onClick = {handleMood} id = {MoodContainer[2]} >
+                    <img src = 'media/Peaceful.svg' alt ='Peaceful'  className = {mood === MoodContainer[2]? 'mood-selected' :'mood-img'}/>
+                    <h4>{MoodContainer[2]}</h4>
+                </div>
+                <div onClick = {handleMood} id = {MoodContainer[3]}>
+                    <img src = 'media/loving.svg' alt ='Loving'  className = {mood === MoodContainer[3]? 'mood-selected' :'mood-img'}/>
+                    <h4>{MoodContainer[3]}</h4>
+                </div>
+                <div onClick = {handleMood} id = {MoodContainer[4]}>
+                    <img src = 'media/stressed.svg' alt ='stressed' className = {mood === MoodContainer[4]? 'mood-selected' :'mood-img'} />
+                    <h4>{MoodContainer[4]}</h4>
+                </div>
+                <div onClick = {handleMood} id = {MoodContainer[5]}>
+                    <img src = 'media/upset.svg' alt ='upset' className = {mood === MoodContainer[5]? 'mood-selected' :'mood-img'} />
+                    <h4>{MoodContainer[5]}</h4>
+                </div>
+                <div  onClick = {handleMood} id = {MoodContainer[6]}>
+                    <img src = 'media/angry.svg' alt ='angry'  className = {mood === MoodContainer[6]? 'mood-selected' :'mood-img'}/>
+                    <h4>{MoodContainer[6]}</h4>
+                </div>
+                <div onClick = {handleMood} id = {MoodContainer[7]}>
+                    <img src = 'media/frustrated.svg' alt ='frustrated'  className = {mood === MoodContainer[7]? 'mood-selected' :'mood-img'}/>
+                    <h4>{MoodContainer[7]}</h4>
+                </div>
+                <div onClick = {handleMood} id = {MoodContainer[8]}>
+                    <img src = 'media/grumpy.svg' alt ='grumpy'  className = {mood === MoodContainer[8]? 'mood-selected' :'mood-img'} />
+                    <h4>{MoodContainer[8]}</h4>
+                </div>
 
-        </section>
-        <section id = "symptoms">
-            <h3>Is there anything else you want to mention?</h3>
-            <textarea placeholder='Type here..' onChange = { (e) => {setSymptoms(symptoms = e.target.value)}}></textarea>
-        </section>
-        <button onClick = {sendData}>Confirm</button>
-      </div>
+            </section>
+            <section id = "symptoms">
+                <h3>Is there anything else you want to mention?</h3>
+                <textarea placeholder='Type here..' onChange = { (e) => {setSymptoms(symptoms = e.target.value)}}></textarea>
+            </section>
+            <button onClick = {sendData}>Confirm</button>
+        </div>
+    </div>
       
 
     )
