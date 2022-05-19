71c83d668d5cc19e514107eac5d4d52879fd945d
wadewilsones <uladaharanina@gmail.com>
2022-05-19 17:27:30 -0400

    more routes was added

--- server.js
+++ server.js
@@ -47,6 +47,15 @@ app.get('/login', (req,res) => {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
 })
 
+app.get('/signup', (req,res) => {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
+})
+
+app.get('/addMood', (req,res) => {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
+})
+
+
 
 app.post('/signUpUser', async (req,res) => {
 
