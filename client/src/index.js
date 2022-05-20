7b0028c200b1307c4365d7e5e01bab66988e222b
wadewilsones <uladaharanina@gmail.com>
2022-05-19 22:44:28 -0400

    test

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 
 
 const [loginData, setLoginData] = useState({
